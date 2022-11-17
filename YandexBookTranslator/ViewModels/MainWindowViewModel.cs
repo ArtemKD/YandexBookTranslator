288d92c3993bd2f39e6b30a4a26e7c560387cf72
Artem <artyomkozlov2001@gmail.com>
2022-11-17 21:54:28 +0300

    Добаление тестовых представлений-моделей и представлений для тестирования переключения контента

---
+++ YandexBookTranslator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using YandexBookTranslator.Infrastructure.Commands;
+using YandexBookTranslator.ViewModels.Base;
+
+namespace YandexBookTranslator.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        public readonly InformationViewModel Information;
+        public readonly TranslateViewModel Translate;
+
+        #region SelectViewModel
+        private object _SelectedViewModel;
+        public object SelectedViewModel
+        {
+            get => _SelectedViewModel;
+            set => Set(ref _SelectedViewModel, value);
+        }
+        #endregion
+
+        #region Commands
+        #region OpenInfoCommand
+        public ICommand OpenInfoCommand { get; }
+        private bool CanOpenInfoCommandExecute(object p) => true;
+        private void OnOpenInfoCommandExecuted(object p)
+        {
+            SelectedViewModel = Information;
+        }
+        #endregion
+
+        #region OpenTransalteCommand
+        public ICommand OpenTransalteCommand { get; }
+        private bool CanOpenTransalteCommandExecute(object p) => true;
+        private void OnOpenTransalteCommandExecuted(object p)
+        {
+            SelectedViewModel = Translate;
+        }
+        #endregion
+        #endregion
+
+        public MainWindowViewModel()
+        {
+
+            OpenInfoCommand = new LambdaCommand(OnOpenInfoCommandExecuted, CanOpenInfoCommandExecute);
+            OpenTransalteCommand = new LambdaCommand(OnOpenTransalteCommandExecuted, CanOpenTransalteCommandExecute);
+
+            Information = new InformationViewModel();
+            Translate = new TranslateViewModel();
+        }
+    }
+}
