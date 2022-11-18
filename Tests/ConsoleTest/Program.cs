d32fedce1e7c64c67b44e8a50ef5b1c7d68caa66
Artem <artyomkozlov2001@gmail.com>
2022-11-18 14:20:20 +0300

    Тестирование словаря поддерживаемых языков

--- Tests/ConsoleTest/Program.cs
+++ Tests/ConsoleTest/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<string, string> dictionary = new Dictionary<string, string>()
+        {
+            {"ru", "Русский" },
+            {"en", "Английский" },
+            {"zh", "Китайский" },
+            {"fr", "Французский" }
+        };
+
+        var keys = dictionary.Keys;
+        var values = dictionary.Values;
+
+        Console.WriteLine(string.Join("\n", keys));
+        Console.WriteLine(string.Join("\n", values));
+    }
+}
