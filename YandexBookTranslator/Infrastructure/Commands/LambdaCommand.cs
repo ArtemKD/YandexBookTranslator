cc4c20bc0d5f0f42cf68c568d443b55ea68853e4
Artem <artyomkozlov2001@gmail.com>
2022-11-18 18:39:55 +0300

    Удаление ненужных подключений библиотек

--- YandexBookTranslator/Infrastructure/Commands/LambdaCommand.cs
+++ YandexBookTranslator/Infrastructure/Commands/LambdaCommand.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using YandexBookTranslator.Infrastructure.Commands.Base;
 
 namespace YandexBookTranslator.Infrastructure.Commands
