35c2312e5f110075f0e7201c979134f6d0d8e08a
Artem <artyomkozlov2001@gmail.com>
2022-11-19 22:02:11 +0300

    Добавление класса для хранения настроек перевода

---
+++ YandexBookTranslator/Models/TranslationFilesInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexBookTranslator.Models
+{
+    internal class TranslationFilesInfo
+    {
+        public string PathFile { get; set; }
+        public string FileName { get; set; }
+        public string SelectedSourceLang { get; set; }
+        public string SelectedTargetLang { get; set; }
+
+        public TranslationFilesInfo(string pathFile, string selectedSourceLang, string selectedTargetLang)
+        {
+            PathFile = pathFile;
+            FileName = pathFile.Split('\\').Last();
+
+            SelectedSourceLang = selectedSourceLang;
+            SelectedTargetLang = selectedTargetLang;
+        }
+    }
+}
